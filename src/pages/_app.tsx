451d0c275fbad8b36eb228abd90fd04be72307f5
Samanta Fluture <samantafluture@gmail.com>
2021-12-29 22:10:29 -0500

    basic props and document configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import '@styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+        document.querySelector('#jss-server-side')?.remove();
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <title>e-diaristas</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/pages/_document.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+    DocumentInitialProps
+} from 'next/document';
+import { ServerStyleSheets } from '@material-ui/styles';
+
+export default class MyDocument extends Document {
+    // define as propriedades iniciais do documento
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
+        const sheets = new ServerStyleSheets(),
+            originalRenderPage = ctx.renderPage;
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => (props) =>
+                    sheets.collect(<App {...props} />)
+            });
+
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return {
+            ...initialProps,
+            styles: [
+                ...React.Children.toArray((await initialProps).styles),
+                sheets.getStyleElement()
+            ]
+        };
+    }
+
+    render(): JSX.Element {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        rel='stylesheet'
+                        href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
