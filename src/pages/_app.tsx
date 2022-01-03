20753a55e5019a1e6605105a5fc50f58f96d3a9e
Samanta Fluture <samantafluture@gmail.com>
2022-01-03 18:13:58 -0500

    configure storybook for theming components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import {ThemeProvider} from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import theme from 'ui/themes/light-theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
