f44574293d93efd03509ee2d182ef03b49b8c857
Samanta Fluture <samantafluture@gmail.com>
2022-01-03 20:04:26 -0500

    create link component w/ storybook

---
+++ src/ui/components/navigation/Link/Link.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import NextLink, { LinkProps as NextLinkProps } from 'next/link';
+import Router from 'next/router';
+import { Link as MuiLink, LinkProps as MuiLinkProps } from '@mui/material';
+
+export interface LinkProps {
+    href: string;
+    mui?: MuiLinkProps;
+    next?: NextLinkProps;
+    Component?: React.ElementType;
+    onClick?: () => void;
+}
+
+const Link: React.FC<LinkProps> = ({
+    children,
+    href,
+    mui,
+    next,
+    Component = MuiLink,
+    ...props
+}) => {
+    const isNextEnv = Boolean(Router.router);
+
+    // se tiver no ambiente do next, retorna um link do next
+    // caso contrário, retorna apenas link do material-ui
+    return isNextEnv ? (
+        <div>
+            <NextLink href={href} passHref {...next}>
+                <Component {...mui} {...props}>
+                    {children}
+                </Component>
+            </NextLink>
+        </div>
+    ) : (
+        <Component href={href} {...mui} {...props}>
+            {children}
+        </Component>
+    );
+};
+
+export default Link;
