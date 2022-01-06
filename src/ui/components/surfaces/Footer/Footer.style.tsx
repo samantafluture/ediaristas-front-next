fb4d12c0e63feac926871a002a821599c3637e41
Samanta Fluture <samantafluture@gmail.com>
2022-01-06 15:02:20 -0500

    create footer for desktop and mobile

--- src/ui/components/surfaces/Footer/Footer.style.tsx
+++ src/ui/components/surfaces/Footer/Footer.style.tsx
@@ -1,5 +1,12 @@
 import { styled } from '@mui/material/styles';
-import { Typography, ListItem } from '@mui/material';
+import {
+    Typography,
+    ListItem,
+    List,
+    Link,
+    IconButton,
+    Container
+} from '@mui/material';
 
 export const FooterContainer = styled('footer')`
     background-color: ${({ theme }) => theme.palette.primary.main};
@@ -13,6 +20,55 @@ export const FooterTitle = styled((props) => (
     font-weight: bold;
 `;
 
+export const FooterSocialList = styled(List)`
+    display: grid;
+    grid-template-columns: repeat(3, 58px);
+    grid-gap: ${({ theme }) => theme.spacing(1.5)};
+`;
+
 export const FooterListItem = styled((props) => (
     <ListItem disableGutters {...props} />
 ))``;
+
+export const SocialContainer = styled('div')`
+    display: flex;
+    flex-direction: column;
+
+    ${({ theme }) => theme.breakpoints.down(726)} {
+        flex-direction: column;
+    }
+`;
+
+export const AppList = styled('ul')`
+    display: flex;
+    gap: ${({ theme }) => theme.spacing()};
+    list-style-type: none;
+    padding: 0;
+    margin: ${({ theme }) => theme.spacing(2) + ' 0 ' + theme.spacing(3)};
+
+    img {
+        width: 112px;
+    }
+`;
+
+export const SocialButton = styled((props) => (
+    <Link
+        component={IconButton}
+        target={'_blank'}
+        rel={'noopener noreferrer'}
+        {...props}
+    />
+))`
+    background-color: ${({ theme }) => theme.palette.primary.dark};
+
+    i {
+        color: ${({ theme }) => theme.palette.primary.contrastText};
+    }
+`;
+
+export const FooterGrid = styled(Container)`
+    display: flex;
+    justify-content: space-between;
+    gap: ${({ theme }) => theme.spacing(2)};
+    flex-wrap: wrap;
+`;
