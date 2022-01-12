3b7b348ef78eacb93634dd5f19c7d0319fcc6d41
Samanta Fluture <samantafluture@gmail.com>
2022-01-12 16:41:37 -0500

    create ui component safe environment

---
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,14 @@
+import { Container } from '@mui/material';
+import { SafeEnvironmentContainer } from './SafeEnvironment.style';
+
+const SafeEnvironment = () => {
+	return (
+		<SafeEnvironmentContainer>
+			<Container>
+				Ambiente 100% Seguro <i className={'twf-lock'} />
+			</Container>
+		</SafeEnvironmentContainer>
+	);
+};
+
+export default SafeEnvironment;
